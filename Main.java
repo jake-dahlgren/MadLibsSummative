a2fcc34d5ed466f4623de25de1bd89bcf9e076b1
Jake Dahlgren <82679852+jake-dahlgren@users.noreply.github.com>
2021-10-08 17:53:45 +0000

    better reprinting of words and comments added

--- Main.java
+++ Main.java
@@ -7,9 +7,14 @@ class Main {
 
     Scanner sc = new Scanner(System.in);
     System.out.println("Enter your words as follows - adjective,adjective,common noun,adjective,common noun,proper noun,proper noun,adjective,adjective. Be sure to seperate each word with commas. ");
-    String userInput = sc.nextLine();
-    System.out.println("Your words were " + userInput + ".");
+    String userInput = sc.nextLine(); //takes user input
   
+/*
+sets the variable for the word equal to the substring of userInput from the begining of 
+the input to the index of the comma. Then the word that has been read and the comma are
+removed so the next word can be read the same way.
+*/
+
     String word1 = userInput.substring(0,userInput.indexOf(","));
     userInput = userInput.replaceFirst(word1 + ",", " ");
 
@@ -34,12 +39,21 @@ class Main {
     String word8 = userInput.substring(0,userInput.indexOf(","));
     userInput = userInput.replaceFirst(word8 + ",", " ");
 
-    String word9 = userInput;
+    String word9 = userInput;  //doesn't clear as it is unnessesary
+
+    System.out.println("Your words were: \n " + word1 + "\n"  //print back user input
+                                          + word2 + "\n"
+                                          + word3 + "\n"
+                                          + word4 + "\n"
+                                          + word5 + "\n"
+                                          + word6 + "\n"
+                                          + word7 + "\n"
+                                          + word8 + "\n"
+                                          + word9 + "\n");
 
 
-
-    String madLib = "The Tyrannosaurus Rex was one of the most -1- dinosaurs to live. Its name meant -2- -3-. It was a meat eater, or carnivore. The T-rex's jaws were very -4- and many -5-s feared it. The T-rex was about 20 feet high, which is as tall as -6-!. It is also over 15000 pounds, which is as heavy as about 7 -7-s! When T-Rex walked, it used its two -8- legs. Even though it was large and scary, its arms were very -9-.";
-    String madLib2 = madLib.replaceAll("-1-", word1);
+    String madLib = "The Tyrannosaurus Rex was one of the most -1- dinosaurs to live. Its name meant -2- -3-. It was a meat eater, or carnivore. The T-rex's jaws were very -4- and many -5-s feared it. The T-rex was about 20 feet high, which is as tall as -6-!. It is also over 15000 pounds, which is as heavy as about 7 -7-s! When T-Rex walked, it used its two -8- legs. Even though it was large and scary, its arms were very -9-.";  //sets madLib to the mad lib
+    String madLib2 = madLib.replaceAll("-1-", word1);  //replace markers within madlib
     madLib2 = madLib2.replaceAll("-2-", word2);
     madLib2 = madLib2.replaceAll("-3-", word3);
     madLib2 = madLib2.replaceAll("-4-", word4);
@@ -48,23 +62,8 @@ class Main {
     madLib2 = madLib2.replaceAll("-7-", word7);
     madLib2 = madLib2.replaceAll("-8-", word8);
     madLib2 = madLib2.replaceAll("-9-", word9);
-    System.out.print(madLib2);
+    System.out.print(madLib2);   //print the final madlib
     
-    /*
-    int tmp = madLib.indexOf("|2|");
-    System.out.println(tmp);
-    madlib = 
-  */
 
-/*
-    System.out.println("Your words split up are:");
-    System.out.println(word1);
-    System.out.println(word2);
-    System.out.println(word3);
-    System.out.println(word4);
-    System.out.println(word5);
-    System.out.println(word6);
-    System.out.println(word7);
-*/
   }
 }
